fd503d9ea5ba0466b25ebdd735ebcadbc806756b
User <User@DESKTOP-1364VBG>
2024-01-29 17:38:54 +0330

    Add project files.

---
+++ tamrin2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace project
+{
+    class test
+    {
+        public static void main()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            name(x, y);
+
+        }
+public static void name (int x , int y)
+            {
+                double volume = Math.Sqrt((Math.Abs(x) + Math.Pow(y, 2)));
+                Console.WriteLine(volume);
+            }
+        
+        }
+
+
+
+    }
+
+
